b17b92496e7f062fc9f977f249b75b3c2854f6f5
Kevin <thekidnamedkd@gmail.com>
2021-01-18 15:56:44 -0800

    configure CI/CD in amplify console

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <h1>Carmen, bienvenidos a Quienst!</h1>
       </header>
       <AmplifySignOut />
     </div>
